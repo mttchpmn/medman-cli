bec2cefcb3a24b594eb6d5ef53d0961849aedb03
Matt Chapman <matt.c@clubware.co.nz>
2020-07-26 16:33:26 +1200

    delete build dir

--- build/validMediaExtensions.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validMediaExtensions = void 0;
-exports.validMediaExtensions = ['.avi', '.mov', '.mkv', '.mp4'];
